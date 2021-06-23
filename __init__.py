27aedfe5331947389d68fb2ef0603ce9507fef30
Arshad Nazir <arshadnzr@email.com>
2021-06-23 15:44:56 +0530

    first

---
+++ __init__.py
@@ -0,0 +1,47 @@
+from . import bfobject
+
+
+class Stream:
+  def __init__(self,client=None,server=None):
+    self.server = server
+    self.client = client
+  
+  
+  def create(self,streamid,source,destination,command,startdata,responsecommand):
+     self.streamid = streamid
+     self.source = source
+     self.destination = destination
+     self.command = command
+     self.startdata = startdata
+     self.responsecommand = responsecommand
+
+
+  
+  def format(self):
+    response = {
+                "streamid" : self.streamid,
+                "source" : self.source,
+                "destination" : self.destination,
+                "command" : self.command,
+                "data" : self.startdata,
+                "responsecommand" : self.responsecommand
+                }
+    return response
+  def format_for_streams(self):
+    response = {
+                "streamid" : self.streamid,
+                "source" : self.source,
+                "destination" : self.destination,
+                "command" : self.command,
+                "responsecommand" : self.responsecommand
+                }
+    return response
+    
+  def push(self,data):
+    obj = Object.Object()
+    packet = {"stream" : self.format_for_streams() , "data" : data} 
+    obj.create(self.destination,"ceo","response","stream", packet)
+    self.client.send(obj.format())
+
+
+
